874424d2fa969162c19532c3e3adf529acbf05ca
wilmer urango <wildau@hotmail.com>
2024-06-01 15:13:14 -0300

    se progr4amo el modelo en el paquete de gurobi

---
+++ gurobi/c_plot.py
@@ -0,0 +1,48 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+import numpy as np
+attrs = ['Authorizations','Assignments']#,'demanda','preco']
+
+def grafica(a, attrs, h, l, ng, p=-1):
+    if p != -1:
+        a = a[a['Periodo']==p]
+
+    dfs = []
+    for attr in attrs:
+        if attr != 'preco':
+            df = pd.pivot_table(a, values=attr, index='o-d', columns=['Vagon','classe'], aggfunc={attr:'sum'})
+        else:
+            df = pd.pivot_table(a, values=attr, index='o-d', columns=['Vagon','classe'], aggfunc={attr:'max'})
+        dfs.append(df)
+
+    fig, ax = plt.subplots(1, ng, figsize=(l, h)) 
+    # plt.figure(figsize=(4, 3))  # Ajusta el ancho a 8 y la altura a 6
+
+    colores = [(0, 'red'), (0.006, 'yellow'), (0.75, 'orange'), (1, 'green')] 
+    cmap_customi = LinearSegmentedColormap.from_list('mi_colormap', colores)
+
+    cont = 0
+    for df in dfs:
+        # Agregar una escala de color usando pcolor
+        ax[cont].pcolor(df, cmap=cmap_customi, edgecolors='w', linewidths=2)
+        # plt.colorbar(ax[cont].pcolor(df, cmap=cmap_customi, edgecolors='w', linewidths=2))
+
+        # Añadir etiquetas de los ejes
+        if cont == 0:
+            ax[cont].set_yticks(np.arange(0.5, len(df.index)), df.index)
+        ax[cont].set_xticks(np.arange(0.5, len(df.columns)), df.columns)
+
+        ax[cont].xaxis.set_ticks_position('top')
+        ax[cont].tick_params(axis='x', rotation=45, labelsize=8)
+
+        # Agregar valores numéricos a cada celda
+        for i in range(len(df.index)):
+            for j in range(len(df.columns)):
+                ax[cont].text(j + 0.5, i + 0.5, str(int(df.iloc[i, j])), color='black', ha='center', va='center')
+
+        # Añadir título y etiquetas
+        ax[cont].set_title(attrs[cont])
+
+        cont += 1
+
+    # fig.show()
---
+++ pyomo/clean_data.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+from itertools import product
+
+def fill_data(oridest, wagons, classes, records_to_correct, periodo=0): #preencha os dados
+     
+    wagon_class = []
+    for v in wagons:
+        wagon_class += list(product([v], classes[v]))
+        
+    try:
+        columnas = ['trecho', 'vagon_clase', 'DBD']
+        df = pd.DataFrame(list(product(oridest, wagon_class, periodo)), columns=columnas)
+    except:
+        columnas = ['trecho', 'vagon_clase']
+        df = pd.DataFrame(list(product(oridest, wagon_class)), columns=columnas)
+
+    df[['Origin', 'Destination']] = pd.DataFrame(df['trecho'].tolist(), index=df.index)
+    df[['Wagon', 'Clase']] = pd.DataFrame(df['vagon_clase'].tolist(), index=df.index)
+    df = df.drop(columns=['trecho', 'vagon_clase'])
+
+    try:
+        df = df[['Origin', 'Destination', 'Wagon', 'Clase', 'DBD']]
+        df['Bookings'] = 0
+        df['clave_completa'] = df['Origin'].astype(str) + '_' + df['Destination'].astype(str) + '_' + df['Wagon'].astype(str) + '_' + df['Clase'].astype(str) + '_' + df['DBD'].astype(str)
+        records_to_correct['clave_completa'] = records_to_correct['Origin'].astype(str) + '_' + records_to_correct['Destination'].astype(str) + '_' + records_to_correct['Wagon'].astype(str) + '_' + records_to_correct['Clase'].astype(str) + '_' + records_to_correct['DBD'].astype(str)
+    except:
+        df['Revenue'] = 0
+        df['clave_completa'] = df['Origin'].astype(str) + '_' + df['Destination'].astype(str) + '_' + df['Wagon'].astype(str) + '_' + df['Clase'].astype(str)
+        records_to_correct['clave_completa'] = records_to_correct['Origin'].astype(str) + '_' + records_to_correct['Destination'].astype(str) + '_' + records_to_correct['Wagon'].astype(str) + '_' + records_to_correct['Clase'].astype(str)
+
+    new_records = df[~df['clave_completa'].isin(records_to_correct['clave_completa'])]
+    records_to_correct = pd.concat([records_to_correct, new_records], ignore_index=True)
+
+    records_to_correct.drop('clave_completa', axis=1, inplace=True)
+        
+    return records_to_correct
+
+
+def clean_data(route, start_estation, commer_class_hierar):
+    # read data
+    data = pd.read_csv(route, delimiter=',')
+    data.drop(columns=['Unnamed: 0'], inplace=True)
+    # data = data[data['DBD']==0]
+
+    # find parameters
+    origin = data['Origin'].unique().tolist()
+    destin = data['Destination'].unique().tolist()
+    periodo = sorted(data['DBD'].unique().tolist(), reverse=True)
+    stations = destin + [i for i in origin if i not in destin]
+
+    # anonymize parameters
+    anonymo_stat = {i: data[data['Market'] == start_estation + '-' + i]['NbLegs'].unique()[0] + 1 if i != start_estation else 1 for i in stations }
+    data.replace(anonymo_stat, inplace=True)
+    data['Market'] = data['Origin'].astype('str') +'-'+ data['Destination'].astype('str')
+    data['CommercialClass'].replace(commer_class_hierar, inplace=True)
+
+    # sort anonymized parameters
+    origin_cor = sorted(data['Origin'].unique().tolist())
+    destin_cor = sorted(data['Destination'].unique().tolist())
+    oridest = data['Market'].unique().tolist()
+    oridest = sorted([(int(i.split('-')[0]), int(i.split('-')[1])) for i in oridest])
+
+    # find parameters
+    data[['Wagon','Clase']] = data['CommercialClass'].str.split('-', expand=True).astype('int')
+    vagones = data['Wagon'].unique().tolist()
+    clases = {v: sorted(data[data['Wagon']==v]['Clase'].unique().tolist()) for v in vagones}
+
+    # Filter demand instance
+    demanda = pd.pivot_table(data, values=['Bookings'], index=['Origin','Destination','Wagon','Clase', 'DBD'], aggfunc={'Bookings':'sum'})
+    demanda.reset_index(inplace=True)
+
+    # Filter revenue instance
+    preco = pd.pivot_table(data, values=['Revenue'], index=['Origin','Destination','Wagon','Clase'], aggfunc={'Revenue':'max'})
+    preco.reset_index(inplace=True)
+
+    # fill instance records
+    preco = fill_data(oridest,vagones,clases, preco,)         
+    demanda = fill_data(oridest,vagones,clases, demanda, periodo)            
+
+    # sort data revenue
+    preco = preco.sort_values(by=['Origin', 'Destination', 'Wagon', 'Revenue'], ascending=[True, True, True, False])
+
+    # Transform to dictionary
+    dem_cor = demanda.set_index(['Origin', 'Destination', 'Wagon', 'Clase', 'DBD'])['Bookings'].to_dict()
+    preco_cor = preco.set_index(['Origin', 'Destination', 'Wagon', 'Clase'])['Revenue'].to_dict()
+
+    return origin_cor, destin_cor, oridest, vagones, periodo, stations, clases, preco_cor, dem_cor, data, anonymo_stat
